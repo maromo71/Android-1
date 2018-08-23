193bced99915536077c547c045ce03e64f411834
gabrielpadilha01 <gabrielpadilhasantos@gmail.com>
2018-08-23 20:45:29 -0300

    Exemplos CalculoSalario

--- CalculoDeSalario/app/src/main/java/faculdade/edu/br/calculodesalario/SalarioActivity.java
+++ CalculoDeSalario/app/src/main/java/faculdade/edu/br/calculodesalario/SalarioActivity.java
@@ -1,5 +1,6 @@
 package faculdade.edu.br.calculodesalario;
 
+import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -27,9 +28,16 @@ public class SalarioActivity extends AppCompatActivity {
             public void onClick(View view) {
                 double salario = Double.parseDouble(txtSalario.getText().toString());
                 int opcao = radioGroupo.getCheckedRadioButtonId();
+                if(opcao == R.id.radio40) salario *= 1.40;
+                if(opcao == R.id.radio45) salario *= 1.45;
+                if(opcao == R.id.radio50) salario *= 1.45;
+                AlertDialog.Builder cxDialogo = new AlertDialog.Builder(SalarioActivity.this);
 
+                cxDialogo.setTitle("Resultado");
+                cxDialogo.setMessage("Novo salário: " + Double.toString(salario));
+                cxDialogo.setNeutralButton("OK",null);
+                cxDialogo.show();
             }
         });
-
     }
 }
