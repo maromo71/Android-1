3197515e432a7632e1e47a9623da7d3ef07f5dba
gabrielpadilha01 <gabrielpadilhasantos@gmail.com>
2018-08-30 20:50:45 -0300

    Trabalho para entrega 01
    Exercicio 03 - Calculo IR

---
+++ CalcularIR/app/src/main/java/faculdade/edu/br/calcularir/MainActivity.java
@@ -0,0 +1,68 @@
+package faculdade.edu.br.calcularir;
+
+import android.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnCalcular;
+    private EditText editSalario;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnCalcular = findViewById(R.id.btnCalcular);
+        editSalario = findViewById(R.id.editSalario);
+
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double salario = Double.parseDouble(editSalario.getText().toString());
+                AlertDialog.Builder cxDialogo = new AlertDialog.Builder(MainActivity.this);
+                cxDialogo.setTitle("Resultado");
+                cxDialogo.setMessage(calcularImposto(salario).toString());
+                cxDialogo.setNeutralButton("OK", null);
+                cxDialogo.show();
+            }
+        });
+    }
+
+    private String calcularImposto(double salario){
+        double valorImposto, salarioLiquido;
+        StringBuilder mensagem = new StringBuilder();
+
+        if(salario < 1257.12) {
+            mensagem.append("Isento de IR");
+            return mensagem.toString();
+        }
+
+        if(salario >= 1257.12 && salario <= 2510) {
+            valorImposto = salario * 0.17;
+            salarioLiquido = salario - valorImposto;
+
+            mensagem.append("Salario bruto: " + Double.toString(salario));
+            mensagem.append("Valor do imposto: " + Double.toString(valorImposto));
+            mensagem.append("Salario Liquido: " + Double.toString(salarioLiquido));
+            return mensagem.toString();
+        }
+
+        if(salario >= 2510 ){
+            valorImposto = salario * 0.28;
+            salarioLiquido = salario - valorImposto;
+
+            mensagem.append("Salario bruto: " + Double.toString(salario));
+            mensagem.append("Valor do imposto: " + Double.toString(valorImposto));
+            mensagem.append("Salario Liquido: " + Double.toString(salarioLiquido));
+            return mensagem.toString();
+        }
+
+        mensagem.append("Erro");
+        return mensagem.toString();
+    };
+}
